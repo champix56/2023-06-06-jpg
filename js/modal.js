57693d68c58d1f0b36070aff493e9e83d41b7f0b
champix56 <desorbaix@free.fr>
2023-06-06 17:10:13 +0100

    modal onject es5

--- js/modal.js
+++ js/modal.js
@@ -1,29 +1,28 @@
-let modalNode = undefined;
-document.addEventListener('DOMContentLoaded', (evt) => {
-    console.log(evt)
-    modalNode = document.querySelector('#modal')
-    console.log(modalNode);
-    removeModal();
-});
-/**
- * fermeture de la modal
- */
-const removeModal = () => {
-    if (undefined !== modalNode) {
-        modalNode.remove()
+function Modal() {
+    var mdl = undefined;
+    function initVar() {
+        document.addEventListener('DOMContentLoaded', (evt) => {
+            console.log(evt)
+            mdl = document.querySelector('#modal')
+            console.log(mdl);
+            removeModal();
+        });
     }
-}
-/**
- * fonction pour afficher une modal
- * @param {string} titre titre du message
- * @param {HTMLElement|string} content contenu html du message
- */
-const showModal = (titre, content) => {
-    if (null === document.querySelector('#modal')) {
-        removeModal();
+    function removeModal() {
+        if (undefined !== modalNode) {
+            modalNode.remove()
+        }
     }
-    modalNode.querySelector('#modal-title').innerHTML = titre
-    modalNode.querySelector('#modal-content').innerHTML = content
+    function showModal(titre, content) {
+        if (null === document.querySelector('#modal')) {
+            removeModal();
+        }
+        mdl.querySelector('#modal-title').innerHTML = titre
+        mdl.querySelector('#modal-content').innerHTML = content
 
-    document.body.appendChild(modalNode);
+        document.body.appendChild(mdl);
+    }
+    this.show=showModal;
+    initVar();
 }
+var modal=new Modal();
