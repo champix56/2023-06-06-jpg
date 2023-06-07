b4dec9fa28c741c90ed261734c389411ca038450
champix56 <desorbaix@free.fr>
2023-06-07 15:55:15 +0100

    metier pour memes et meme

---
+++ js/meme.js
@@ -0,0 +1,47 @@
+class Meme {
+    static ressourceName = '/memes'
+    titre = ""
+    text = ""
+    x = 0
+    y = 20
+    fontWeight = "500"
+    fontSize = 20
+    underline = false
+    italic = false
+    imageId = -1
+    color = "#000000"
+    frameSizeX = 0
+    frameSizeY = 0
+    save = () => { }
+    deserialize = (genriqueObject) => {
+
+    }
+}
+class Memes extends Array {
+    static ressourceName = '/memes'
+    constructor() {
+        super();
+    }
+    push = (values) => {
+        if (Array.isArray(values)) {
+            values.forEach(value => super.push(value))
+        }
+        else {
+            super.push(values)
+        }
+    }
+    load = () => {
+        return fetch(`${REST_ADR}${Memes.ressourceName}`)
+            .then(flux => flux.json())
+            .then(arr => {
+                arr.forEach(unique => {
+                    const meme = new Meme()
+                    Object.assign(meme, unique)
+                    console.log(meme)
+                    this.push(Object.freeze(meme))
+                })
+                return arr
+            })
+    }
+}
+const listeMemes = new Memes()
