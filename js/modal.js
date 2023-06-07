51ec8a358d274efcdb6e58a2fcc6c3c362199a15
champix56 <desorbaix@free.fr>
2023-06-07 08:54:02 +0100

    passage en classe pour la modal

--- js/modal.js
+++ js/modal.js
@@ -1,43 +1,37 @@
-/**
- * Objet pour la gestion de la modal en version es5
- */
-function Modal() {
-    /**
-     * ref principal de la modal
-     */
-    var mdl = undefined;
-    /**
-     * initialisation de l'objet modal
-     */
-    function initVar() {
-        document.addEventListener('DOMContentLoaded', (evt) => {
-            console.log(evt)
-            mdl = document.querySelector('#modal')
-            console.log(mdl);
-            removeModal();
-        });
-    }
-    /**
-     * fonction privée de suppression de la modal
-     */
-    function removeModal() {
-        if (undefined !== modalNode) {
-            modalNode.remove()
-        }
-    }
-    function showModal(titre, content) {
-        if (null === document.querySelector('#modal')) {
-            removeModal();
-        }
-        mdl.querySelector('#modal-title').innerHTML = titre
-        mdl.querySelector('#modal-content').innerHTML = content
 
-        document.body.appendChild(mdl);
+
+
+
+class Modal {
+    #refId;
+    #modalNode;
+    constructor(idModal = "modal") {
+        this.#refId = idModal
+        document.addEventListener('DOMContentLoaded', (evt) => {
+            this.#modalNode = document.querySelector(`#${this.#refId}`)
+            this.#removeModal()
+        })
     }
     /**
-     * exposition de la fonction d'affichage de la modal
-     */
-    this.show=showModal;
-    initVar();
+    * fermeture de la modal
+    */
+    #removeModal = () => {
+        if (undefined !== this.#modalNode) {
+            this.#modalNode.remove()
+        }
+    }
+    /**
+ * fonction pour afficher une modal
+ * @param {string} titre titre du message
+ * @param {HTMLElement|string} content contenu html du message
+ */
+ showModal = (titre, content) => {
+    if (null !== this.#modalNode) {
+        this.#removeModal()
+    }
+    this.#modalNode.querySelector('#modal-title').innerHTML = titre
+    this.#modalNode.querySelector('#modal-content').innerHTML = content
+    document.body.appendChild(this.modalNode)
 }
-var modal=new Modal();
+}
+const modal=new Modal('modal')
