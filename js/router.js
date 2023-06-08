27c4ff7191f4bf884d025c6bf7202651a2a679db
champix56 <desorbaix@free.fr>
2023-06-08 10:36:35 +0100

    fix route object def

--- js/router.js
+++ js/router.js
@@ -1,18 +1,19 @@
 
-const routes = {
-    thumbnail: '/thumbnail',
-    home: '/'
-}
+const routes = [
+    { pathName: '/meme', viewUrl: '/views/editor.html', pathRegex: /\/meme\/*/ },
+    { pathName: '/thumbnail', viewUrl: '/views/thumbnail.html', pathRegex: /\/thumbnail/ },
+    { pathName: '/', viewUrl: '/views/home.html', pathRegex: /\/*/ },
+]
 
 export class RouterDOM {
     #currentUrl
-    set currentRoute(urlStr) { 
-        window.history.pushState(null,null,urlStr)
+    set currentRoute(urlStr) {
+        window.history.pushState(null, null, urlStr)
     }
     constructor() {
-        this.#currentUrl=window.location.pathname
+        this.#currentUrl = window.location.pathname
     }
-    manageRoute=()=>{
+    manageRoute = () => {
 
     }
 }
