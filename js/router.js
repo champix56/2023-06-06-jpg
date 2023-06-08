e2b7757911baae08a4fdc5b2cab115a59ef19911
champix56 <desorbaix@free.fr>
2023-06-08 15:10:09 +0100

    gestion complete de l'integration de route

--- js/router.js
+++ js/router.js
@@ -1,8 +1,18 @@
+import { listeImgs, listeMemes } from "./coreLib/dataInstance.js"
+
 
 const routes = [
-    { pathName: '/meme', viewUrl: '/views/editor.html', pathRegex: /^\/meme(\/(?<id>\d{0,})?)?\/?$/ },
-    { pathName: '/thumbnail', viewUrl: '/views/thumbnail.html', pathRegex: /^\/thumbnail\/?$/ },
-    { pathName: '/', viewUrl: '/views/home.html', pathRegex: /^\/(home)?\/?$/ },
+    {
+        name:'editor', 
+        pathName: '/meme', 
+        viewUrl: '/views/editor.html', 
+        pathRegex: /^\/meme(\/(?<id>\d{0,})?)?\/?$/ 
+    },
+    {
+        name:'thumb', pathName: '/thumbnail', viewUrl: '/views/thumbnail.html', pathRegex: /^\/thumbnail\/?$/,
+        data:{memes:listeMemes, images:listeImgs} 
+    },
+    {name:'home', pathName: '/', viewUrl: '/views/home.html', pathRegex: /^\/(home)?\/?$/ },
 ]
 export class RouterDOM {
     #currentUrl
@@ -10,6 +20,7 @@ export class RouterDOM {
     currentParams
     set currentRoute(urlStr) {
         window.history.pushState(null, null, urlStr)
+        this.manageRoute()
     }
     constructor() {
         this.#currentUrl = window.location.pathname
@@ -24,5 +35,26 @@ export class RouterDOM {
                 return true
             }
         })
+        //let templateText=sessionStorage.getItem(this.#currentRoute.name)
+        if(undefined!==this.#currentRoute.templateText){
+            this.#wrapTemplate(this.#currentRoute)
+        }
+        else {
+            this.#loadTemplate(this.#currentRoute)
+        }
+    }
+
+    #loadTemplate=(route)=>{
+        fetch(route.viewUrl)
+            .then(f=>f.text())
+            .then(text=>{
+                //sessionStorage.setItem(route.name,text);
+                this.#currentRoute.templateText=text
+                this.#wrapTemplate(this.#currentRoute);
+            })
+    }
+    #wrapTemplate=(route)=>{
+        const wrapper=document.querySelector('#main-wrapper')
+        wrapper.innerHTML=route.templateText;
     }
 }
